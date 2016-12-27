32f61687e107f2b850c12f5bda3d664027e25812
Glyn Hudson <glyn.hudson@gmail.com>
2016-12-27 11:36:34 +0000

    breakout config, add schedule

--- leaf-python-mqtt.py
+++ leaf-python-mqtt.py
@@ -7,100 +7,105 @@ import logging
 import sys
 import pprint
 import paho.mqtt.client as mqtt
+import schedule
+from datetime import datetime
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.info("Startup leaf status " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+# Get login details from 'config.ini'
+parser = SafeConfigParser()
+candidates = [ 'config.ini', 'my_config.ini' ]
+found = parser.read(candidates)
+username = parser.get('get-leaf-info', 'username')
+password = parser.get('get-leaf-info', 'password')
+mqtt_host = parser.get('get-leaf-info', 'mqtt_host')
+mqtt_port = parser.get('get-leaf-info', 'mqtt_port')
+mqtt_username = parser.get('get-leaf-info', 'mqtt_username')
+mqtt_password = parser.get('get-leaf-info', 'mqtt_password')
+mqtt_control_topic = parser.get('get-leaf-info', 'mqtt_control_topic')
+mqtt_status_topic =  parser.get('get-leaf-info', 'mqtt_status_topic')
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-  client.subscribe("emon/leaf/control/#")
-  # leaf/control/ac/ 1 to turn on AC
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.publish("emonesp/test", "openevse connected");
+  logging.info("Connected to MQTT with result code "+str(rc))
+  logging.info("suscribing to leaf control topic" + mqtt_control_topic)
+  client.subscribe(mqtt_control_topic + "/#")
+  client.publish(mqtt_status_topic, "leaf status connected to MQTT");
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
   
-    print(msg.topic+" "+str(msg.payload))
+    logging.info(msg.topic+" "+str(msg.payload))
     
     # If climate control messaage is received
-    if msg.topic.rsplit('/',1)[0] == 'ac':
+    if msg.topic.rsplit('/',1)[0] == 'climate':
       if msg.payload == 1:
         print "Turing on climate control"
       if msg.payload == 0:
         print "Turing off climate control"
-      
-        
 
 client = mqtt.Client()
+# Callback when MQTT is connected
 client.on_connect = on_connect
+# Callback when MQTT message is received
 client.on_message = on_message
+# Connect to MQTT
+client.username_pw_set(mqtt_username, mqtt_password);
+client.connect(mqtt_host, mqtt_port, 60)
 
-client.username_pw_set('emonpi', 'emonpimqtt2016');
-client.connect("localhost", 1883, 60)
+def get_leaf_status():
+  logging.debug("login = %s , password = %s" % ( username , password)  )
+  print "Prepare Session"
+  s = pycarwings2.Session(username, password , "NE")
+  print "Login..."
+  print datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  l = s.get_leaf()
+  
+  print "get_latest_battery_status"
+  leaf_info = l.get_latest_battery_status()
+  print "date %s" % leaf_info.answer["BatteryStatusRecords"]["OperationDateAndTime"]
+  print "date %s" % leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"]
+  print "battery_capacity2 %s" % leaf_info.answer["BatteryStatusRecords"]["BatteryStatus"]["BatteryCapacity"]
+  
+  print "battery_capacity %s" % leaf_info.battery_capacity
+  print "charging_status %s" % leaf_info.charging_status
+  print "battery_capacity %s" % leaf_info.battery_capacity
+  print "battery_remaining_amount %s" % leaf_info.battery_remaining_amount
+  print "charging_status %s" % leaf_info.charging_status
+  print "is_charging %s" % leaf_info.is_charging
+  print "is_quick_charging %s" % leaf_info.is_quick_charging
+  print "plugin_state %s" % leaf_info.plugin_state
+  print "is_connected %s" % leaf_info.is_connected
+  print "is_connected_to_quick_charger %s" % leaf_info.is_connected_to_quick_charger
+  print "time_to_full_trickle %s" % leaf_info.time_to_full_trickle
+  print "time_to_full_l2 %s" % leaf_info.time_to_full_l2
+  print "time_to_full_l2_6kw %s" % leaf_info.time_to_full_l2_6kw
+  print "leaf_info.battery_percent %s" % leaf_info.battery_percent
+  
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-# client.publish("emon/leaf/status", "connected");
-# client.loop_forever()
+  client.publish(mqtt_status_topic + "/last_updated", leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"])
+  time.sleep(1)
+  client.publish(mqtt_status_topic + "/battery_percent", leaf_info.battery_percent)
+  time.sleep(1)
+  client.publish(mqtt_status_topic + "/charging_status", leaf_info.charging_status)
+  time.sleep(1)
+  client.publish(mqtt_status_topic + "/connected", leaf_info.is_connected)
 
 
 
+schedule.every(1).minutes.do(get_leaf_status)
 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
+    # MQTT loop
+    # client.loop_forever()
 
-
-
-
-
-logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
-
-
-parser = SafeConfigParser()
-candidates = [ 'config.ini', 'my_config.ini' ]
-found = parser.read(candidates)
-
-username = parser.get('get-leaf-info', 'username')
-password = parser.get('get-leaf-info', 'password')
-
-logging.debug("login = %s , password = %s" % ( username , password)  )
-
-print "Prepare Session"
-s = pycarwings2.Session(username, password , "NE")
-print "Login..."
-l = s.get_leaf()
-
-print "get_latest_battery_status"
-leaf_info = l.get_latest_battery_status()
-print "date %s" % leaf_info.answer["BatteryStatusRecords"]["OperationDateAndTime"]
-print "date %s" % leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"]
-print "battery_capacity2 %s" % leaf_info.answer["BatteryStatusRecords"]["BatteryStatus"]["BatteryCapacity"]
-
-print "battery_capacity %s" % leaf_info.battery_capacity
-print "charging_status %s" % leaf_info.charging_status
-print "battery_capacity %s" % leaf_info.battery_capacity
-print "battery_remaining_amount %s" % leaf_info.battery_remaining_amount
-print "charging_status %s" % leaf_info.charging_status
-print "is_charging %s" % leaf_info.is_charging
-print "is_quick_charging %s" % leaf_info.is_quick_charging
-print "plugin_state %s" % leaf_info.plugin_state
-print "is_connected %s" % leaf_info.is_connected
-print "is_connected_to_quick_charger %s" % leaf_info.is_connected_to_quick_charger
-print "time_to_full_trickle %s" % leaf_info.time_to_full_trickle
-print "time_to_full_l2 %s" % leaf_info.time_to_full_l2
-print "time_to_full_l2_6kw %s" % leaf_info.time_to_full_l2_6kw
-print "leaf_info.battery_percent %s" % leaf_info.battery_percent
-
-client.publish("emon/leaf/status/test", "publishing status")
-time.sleep(1)
-client.publish("emon/leaf/status/last_updated", leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"])
-time.sleep(1)
-client.publish("emon/leaf/status/battery_percent", leaf_info.battery_percent)
-time.sleep(1)
-client.publish("emon/leaf/status/charging_status", leaf_info.charging_status)
-time.sleep(1)
-client.publish("emon/leaf/status/connected", leaf_info.is_connected)
+    
+    
+    
 
 # result_key = l.request_update()
 # print "start sleep 10"
