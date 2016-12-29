20362ad8f9a751de2c007afa68e3692f7b3fd7a6
Glyn Hudson <glyn.hudson@gmail.com>
2016-12-29 13:37:14 +0000

    add update and climate control option

--- leaf-python-mqtt.py
+++ leaf-python-mqtt.py
@@ -11,6 +11,7 @@ import schedule
 from datetime import datetime
 import os
 
+
 config_file = 'config.ini'
 
 
@@ -33,6 +34,7 @@ if os.path.exists(config_file_path):
   mqtt_password = parser.get('get-leaf-info', 'mqtt_password')
   mqtt_control_topic = parser.get('get-leaf-info', 'mqtt_control_topic')
   mqtt_status_topic =  parser.get('get-leaf-info', 'mqtt_status_topic')
+  GET_UPDATE_INTERVAL = parser.get('get-leaf-info', 'api_update_interval_min')
 else:
   logging.error("ERROR: Config file not found " + config_file_path)
   quit()
@@ -41,22 +43,37 @@ else:
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
   logging.info("Connected to MQTT with result code "+str(rc))
-  logging.info("suscribing to leaf control topic" + mqtt_control_topic)
+  logging.info("suscribing to leaf control topic: " + mqtt_control_topic)
   client.subscribe(mqtt_control_topic + "/#")
-  client.publish(mqtt_status_topic, "leaf status connected to MQTT");
+  client.publish(mqtt_status_topic, "MQTT connected");
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
   
     logging.info(msg.topic+" "+str(msg.payload))
-    
-    # If climate control messaage is received
-    if msg.topic.rsplit('/',1)[0] == 'climate':
-      if msg.payload == 1:
-        print "Turing on climate control"
-      if msg.payload == 0:
-        print "Turing off climate control"
 
+    control_subtopic = msg.topic.rsplit('/',1)[1]
+    control_message = msg.payload
+    logging.info("control sub-topic: " + control_subtopic)
+    logging.info("control message: " + control_message)
+
+    # If climate control messaage is received mqtt_control_topic/climate
+    if control_subtopic == 'climate':
+      logging.info('Climate control command received: ' + control_message)
+      
+      if control_message == '1':
+        climate_control(1)
+
+      if control_message == '0':
+        climate_control(0)
+        
+    # If climate control messaage is received on mqtt_control_topic/update
+    if control_subtopic == 'update':
+      logging.info('Update control command received: ' + control_message)
+      if control_message == '1':
+        get_leaf_update()
+        get_leaf_status()
+        
 client = mqtt.Client()
 # Callback when MQTT is connected
 client.on_connect = on_connect
@@ -65,37 +82,91 @@ client.on_message = on_message
 # Connect to MQTT
 client.username_pw_set(mqtt_username, mqtt_password);
 client.connect(mqtt_host, mqtt_port, 60)
+client.publish(mqtt_status_topic, "Connecting to MQTT...");
 
-def get_leaf_status():
+
+
+def climate_control(climate_control_instruction):
   logging.debug("login = %s , password = %s" % ( username , password)  )
-  print "Prepare Session"
+  logging.info("Prepare Session climate control update")
   s = pycarwings2.Session(username, password , "NE")
-  print "Login..."
-  print datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  logging.info("Login...")
+  logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
   l = s.get_leaf()
   
-  print "get_latest_battery_status"
-  leaf_info = l.get_latest_battery_status()
-  print "date %s" % leaf_info.answer["BatteryStatusRecords"]["OperationDateAndTime"]
-  print "date %s" % leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"]
-  print "battery_capacity2 %s" % leaf_info.answer["BatteryStatusRecords"]["BatteryStatus"]["BatteryCapacity"]
+  if climate_control_instruction == 1:
+    logging.info("Turning on climate control..wait 60s")
+    result_key = l.start_climate_control()
+    time.sleep(60)
+    #start_cc_result = l.get_start_climate_control_result(result_key)
+    #logging.info(start_cc_result)
+    logging.info("climate turned on, unable to get statuts")
   
-  print "battery_capacity %s" % leaf_info.battery_capacity
-  print "charging_status %s" % leaf_info.charging_status
-  print "battery_capacity %s" % leaf_info.battery_capacity
-  print "battery_remaining_amount %s" % leaf_info.battery_remaining_amount
-  print "charging_status %s" % leaf_info.charging_status
-  print "is_charging %s" % leaf_info.is_charging
-  print "is_quick_charging %s" % leaf_info.is_quick_charging
-  print "plugin_state %s" % leaf_info.plugin_state
-  print "is_connected %s" % leaf_info.is_connected
-  print "is_connected_to_quick_charger %s" % leaf_info.is_connected_to_quick_charger
-  print "time_to_full_trickle %s" % leaf_info.time_to_full_trickle
-  print "time_to_full_l2 %s" % leaf_info.time_to_full_l2
-  print "time_to_full_l2_6kw %s" % leaf_info.time_to_full_l2_6kw
-  print "leaf_info.battery_percent %s" % leaf_info.battery_percent
+  if climate_control_instruction == 0:
+    logging.info("Turning off climate control..wait 60s")
+    result_key = l.stop_climate_control()
+    time.sleep(60)
+    #stop_cc_result = l.get_stop_climate_control_result(result_key)
+    #logging.info(stop_cc_result)
+    logging.info("climate turned off, unable to get statuts")
+
   
 
+def get_leaf_update():
+  logging.debug("login = %s , password = %s" % ( username , password)  )
+  logging.info("Prepare Session get car update")
+  s = pycarwings2.Session(username, password , "NE")
+  logging.info("Login...")
+  logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+  l = s.get_leaf()
+  logging.info("Requesting update from car..wait 30s")
+  result_key = l.request_update()
+  time.sleep(30)
+  battery_status = l.get_status_from_update(result_key)
+  
+  while battery_status is None:
+    logging.error("ERROR: no responce from car")
+    time.sleep(10)
+    battery_status = l.get_status_from_update(result_key)
+  
+  leaf_info = l.get_latest_battery_status()
+
+  
+def get_leaf_status():
+  logging.debug("login = %s , password = %s" % ( username , password)  )
+  logging.info("Prepare Session")
+  s = pycarwings2.Session(username, password , "NE")
+  logging.info("Login...")
+  logging.info(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+  l = s.get_leaf()
+  
+  logging.info("get_latest_battery_status")
+  
+  leaf_info = l.get_latest_battery_status()
+  
+  logging.info("date %s" % leaf_info.answer["BatteryStatusRecords"]["OperationDateAndTime"])
+  logging.info("date %s" % leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"])
+  logging.info("battery_capacity2 %s" % leaf_info.answer["BatteryStatusRecords"]["BatteryStatus"]["BatteryCapacity"])
+  logging.info("battery_capacity %s" % leaf_info.battery_capacity)
+  logging.info("charging_status %s" % leaf_info.charging_status)
+  logging.info("battery_capacity %s" % leaf_info.battery_capacity)
+  logging.info("battery_remaining_amount %s" % leaf_info.battery_remaining_amount)
+  logging.info("charging_status %s" % leaf_info.charging_status)
+  logging.info("is_charging %s" % leaf_info.is_charging)
+  logging.info("is_quick_charging %s" % leaf_info.is_quick_charging)
+  logging.info("plugin_state %s" % leaf_info.plugin_state)
+  logging.info("is_connected %s" % leaf_info.is_connected)
+  logging.info("is_connected_to_quick_charger %s" % leaf_info.is_connected_to_quick_charger)
+  logging.info("time_to_full_trickle %s" % leaf_info.time_to_full_trickle)
+  logging.info("time_to_full_l2 %s" % leaf_info.time_to_full_l2)
+  logging.info("time_to_full_l2_6kw %s" % leaf_info.time_to_full_l2_6kw)
+  logging.info("leaf_info.battery_percent %s" % leaf_info.battery_percent)
+  
+  # logging.info("getting climate update")
+  # climate = l.get_latest_hvac_status()
+  # pprint.pprint(climate)
+
+  logging.info("publishing to MQTT base status topic: " + mqtt_status_topic)
   client.publish(mqtt_status_topic + "/last_updated", leaf_info.answer["BatteryStatusRecords"]["NotificationDateAndTime"])
   time.sleep(1)
   client.publish(mqtt_status_topic + "/battery_percent", leaf_info.battery_percent)
@@ -115,35 +186,12 @@ def get_leaf_status():
 get_leaf_status()
 
 # Then schedule
-schedule.every(30).minutes.do(get_leaf_status)
+logging.info("Schedule leaf update every " + GET_UPDATE_INTERVAL + "min")
+schedule.every(GET_UPDATE_INTERVAL).minutes.do(get_leaf_status)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
     
     # MQTT loop
-    # client.loop_forever()
-
-    
-    
-    
-
-# result_key = l.request_update()
-# print "start sleep 10"
-# time.sleep(10) # sleep 60 seconds to give request time to process
-# print "end sleep 10"
-# battery_status = l.get_status_from_update(result_key)
-# while battery_status is None:
-# 	print "not update"
-#         time.sleep(10)
-# 	battery_status = l.get_status_from_update(result_key)
-
-# pprint.pprint(battery_status.answer)
-
-#result_key = l.start_climate_control()
-#time.sleep(60)
-#start_cc_result = l.get_start_climate_control_result(result_key)
-
-#result_key = l.stop_climate_control()
-#time.sleep(60)
-#stop_cc_result = l.get_stop_climate_control_result(result_key)
+    #client.loop_forever()
